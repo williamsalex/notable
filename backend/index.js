84df349ee1ef61df6f8a3b4a746512289b049466
williamsalex <williams_alex@protonmail.com>
2021-07-11 11:05:08 -0500

    Added appointment creation functionality

--- backend/index.js
+++ backend/index.js
@@ -35,35 +35,69 @@ app.get('/appointments/:doctorId/:day', (req, res) => {
   let data = fs.readFileSync('./db.json', 'utf8')
   let appointments = JSON.parse(data).appointments
 
-  appointments = appointments.filter((apt) => apt.day === day && apt.doctor_id === doctorId)
+  let appointmentsOnDay = Object.keys(appointments).filter((id) => appointments[id].day === day && appointments[id].doctor_id === doctorId)
+  appointmentsOnDay = appointmentsOnDay.map((apt) => appointments[apt])
 
-  res.send(appointments)
+  res.send(appointmentsOnDay)
 
 })
 
-app.post('/appointments/new/:doctorId/:day/:time', (req, res) => {
+app.post('/appointments/new/:doctorId/:patientId/:day/:time/:kind', (req, res) => {
+
+  let doctorId = req.params.doctorId
+  let patientId = req.params.patientId
+  let day = req.params.day
+  let time = req.params.time
+  let kind = req.params.kind
+
+  // Checking if the appointment is on a 15 minute interval before we load the database
+
+  if (parseInt(time.split(':')[1]) % 15 !== 0) { res.sendStatus(422) }
 
   let data = fs.readFileSync('./db.json', 'utf8')
-  let currCount
 
-  // If the file is just starting, initialize at 0, and increment to one
+  let currentState = JSON.parse(data)
 
-  if (data === '') {
+  let doctorExists = currentState.doctors[doctorId]
 
-    currCount = {clicks: 0}
+  let patientExists = currentState.patients[patientId]
 
-  } else {
+  if (!doctorExists || !patientExists) { res.sendStatus(422) }
 
-    currCount = JSON.parse(data)
+  // Checking that the doctor doesn't have too many appointments at the desired time
+
+  let appointmentsByDay = currentState.appointments_by_day[day]
+  let doctorAppointmentsByDay
+  let appointmentsAtDesiredTime
+  
+  if (appointmentsByDay && appointmentsByDay.length) {
+
+    doctorAppointmentsByDay = appointmentsByDay.map((apt) => currentState.appointments[apt])
+
+    appointmentsAtDesiredTime = doctorAppointmentsByDay.filter((apt) => apt.time === time)
 
   }
 
-  currCount.clicks ++
+  if (appointmentsAtDesiredTime > 2) { res.sendStatus(422) }
 
-  let content = JSON.stringify(currCount)
+  // This is horrible but fast - need to at least implement a semaphore
 
-  fs.writeFileSync('./db.json', content)
+  let appointmentId = currentState.appointments_curr_max_id + 1
+
+  currentState.appointments[appointmentId] = {"doctor_id": doctorId, "patient_id": patientId, "day": day, "time": time, "kind": kind}
+
+  fs.writeFileSync('./db.json', JSON.stringify(currentState))
   res.sendStatus(200)
+
+})
+
+app.delete('appointments/:id', (req, res) => {
+
+  let appointmentId = req.params.id
+
+  let data = fs.readFileSync('./db.json', 'utf8')
+
+  let currentState = JSON.parse(data)
 
 })
 
